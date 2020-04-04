8ec09c9e9520789be17591406a9067526e44b418
Sebastian Baquero <sebastian_290794@hotmail.com>
2020-04-04 12:43:54 -0500

    crea ruta de usuarios

--- App/appCrud/src/app/users/users-routing.module.ts
+++ App/appCrud/src/app/users/users-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserListComponent } from '../users/user-list/user-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: UserListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
